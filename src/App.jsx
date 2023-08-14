22e4ca3506e64bf9f5eec8c89646b99df4b060f1
Patrick Fien <pmfien99@icloud.com>
2023-08-14 15:34:49 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Register from "./pages/Register";
+import "../src/style.scss"
+import Home from "./pages/Home";
+function App() {
+  return (
+      <Home />
+  );
+}
+
+export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Add from "../img/addAvatar.png"
+const Register = () => {
+  return (
+    <div className='formContainer'>
+      <div className='formWrapper'>
+      <span className='logo'>Lama Chat</span>
+      <span className='title'>Register</span>
+        <form>
+          <input type="text" placeholder='dispaly name'/>
+          <input type="email" placeholder='email'/>
+          <input type="password" placeholder='password'/>
+          <input style={{display: "none"}} type="file" id="file" />
+          <label htmlFor='file'>
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>Aleady have an account? Login</p>
+      </div>
+    </div>
+  )
+}
+
+export default Register
